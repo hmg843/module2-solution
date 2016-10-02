627f5d72103d255b2a2fa0d4786a4707bb098fbc
hmugasha <hmug@hotmail.com>
2016-10-02 02:10:24 +0000

    initial commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,67 @@
 (function(){
   'ue strict';
 
-  angular.module('LunchCheck', [])
-         .controller('LunchCheckController', LunchCheckController);
+  angular.module('ShoppingListCheckOff', [])
+         .controller('ToBuyShoppingController', ToBuyShoppingController)
+         .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  LunchCheckController.$inject = ["$scope"];
-  function LunchCheckController($scope)
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+  AlreadyBoughtShoppingController.$inject= ['ShoppingListCheckOffService'];
+
+  function ToBuyShoppingController(ShoppingListCheckOffService)
   {
-      $scope.lunchMenu = "";
-      $scope.lunchMenuCheckResult = "";
-      $scope.CheckLunchMenu = function() {
-        var numberOfItems = 0;
-        var lunchMenu = $scope.lunchMenu;
-        var lunchItems = lunchMenu.split(",");
-        for(i=0; i<lunchItems.length; i++) {
-          if(lunchItems[i].trim().length > 0) {
-            numberOfItems++;
-          }
-        }
-        if(numberOfItems > 3) {
-          $scope.lunchMenuCheckResult = "Too Much!";
-        } else if(numberOfItems > 0) {
-          $scope.lunchMenuCheckResult = "Enjoy!";
-        } else {
-          $scope.lunchMenuCheckResult = "";
-        }
-      };
+    var ItemsToBuy = this;
+    ItemsToBuy.BuyItem = function(index)
+    {
+        ShoppingListCheckOffService.BuyItem(index);
+    }
+    ItemsToBuy.ShoppingListEmpty = function()
+    {
+      return ShoppingListCheckOffService.ItemsToBuyCount() <= 0;
+    }
+    ItemsToBuy.ShoppingList = function()
+    {
+      return ShoppingListCheckOffService.ItemsToBuy;
+    }
   };
+
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService)
+  {
+    var ItemsBought = this;
+    ItemsBought.PurchasedItemsEmpty= function()
+    {
+      return ShoppingListCheckOffService.ItemsBoughtCount() <= 0;
+    }
+    ItemsBought.PurchasedList = function()
+    {
+      return ShoppingListCheckOffService.ItemsBought;
+    }
+  }
+
+  function ShoppingListCheckOffService()
+  {
+    var service = this;
+    service.ItemsBought = [];
+    service.ItemsToBuy  = [
+      { name: "cookies",     quantity: 10 },
+      { name: "crackers",    quantity: 3 },
+      { name: "Coke",        quantity: 7 },
+      { name: "Beef Jerkey", quantity: 12 },
+      { name: "Hot Dog",     quantity: 5 },
+    ];
+    service.BuyItem = function(index)
+    {
+      service.ItemsBought.push(service.ItemsToBuy[index]);
+      service.ItemsToBuy.splice(index, 1);
+    }
+    service.ItemsToBuyCount = function()
+    {
+      return service.ItemsToBuy.length;
+    }
+    service.ItemsBoughtCount = function()
+    {
+      return service.ItemsBought.length;
+    }
+  }
 })();
